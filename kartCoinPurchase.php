749c8a8a255332ae230a7a1c53a83556dd3d54bf
taggaddaaaa <scaizerg@student.42.fr>
2019-06-24 12:54:07 +0200

    add kartCoin purchase content page

---
+++ kartCoinPurchase.php
@@ -0,0 +1,126 @@
+<table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" class="templateContainer">
+	<tr>
+		<!--CONTENT-->
+		<td valign="top" class="bodyContainer">
+			<!--TITLE-->
+			<table border="0" cellpadding="0" cellspacing="0" width="100%"
+			       class="mcnTextBlock" style="min-width:100%;">
+				<tbody class="mcnTextBlockOuter">
+				<tr>
+					<td valign="top" class="mcnTextBlockInner" style="padding-top:9px;">
+						<table align="left" border="0" cellpadding="0" cellspacing="0"
+						       style="max-width:100%; min-width:100%;" width="100%"
+						       class="mcnTextContentContainer">
+							<tbody>
+							<tr>
+								<td valign="top" class="mcnTextContent" style="padding: 0 18px 9px;">
+									<h4>youpiiiii...</h4>
+									<h1>Ton achat t'attends au circuit</h1>
+								</td>
+							</tr>
+							</tbody>
+						</table>
+					</td>
+				</tr>
+				</tbody>
+			</table>
+			<!--SPACE-->
+			<table border="0" cellpadding="0" cellspacing="0" width="100%"
+			       class="mcnDividerBlock" style="min-width:100%;">
+				<tbody class="mcnDividerBlockOuter">
+				<tr>
+					<td class="mcnDividerBlockInner"
+					    style="min-width: 100%; padding: 27px 18px 0px;">
+						<table class="mcnDividerContent" border="0" cellpadding="0"
+						       cellspacing="0" width="100%" style="min-width:100%;">
+							<tbody>
+							<tr>
+								<td>
+									<span></span>
+								</td>
+							</tr>
+							</tbody>
+						</table>
+					</td>
+				</tr>
+				</tbody>
+			</table>
+			<!--IMAGES-->
+			<table border="0" cellpadding="0" cellspacing="0" width="100%"
+			       class="mcnImageGroupBlock">
+				<tbody class="mcnImageGroupBlockOuter">
+				<!--1 img centered-->
+				<tr>
+					<td valign="top" style="padding:9px;" class="mcnImageGroupBlockInner">
+
+						<table align="left" width="564" border="0" cellpadding="0"
+						       cellspacing="0" class="mcnImageGroupContentContainer">
+							<tbody>
+							<tr>
+								<td class="mcnImageGroupContent" valign="top" align="center"
+								    style="padding-left: 9px; padding-top: 0; padding-bottom: 0;">
+									<img src="img/kartCoin.png" alt="kartCoin">
+								</td>
+							</tr>
+							</tbody>
+						</table>
+
+					</td>
+				</tr>
+				</tbody>
+			</table>
+			<!--SPACE-->
+			<table border="0" cellpadding="0" cellspacing="0" width="100%"
+			       class="mcnDividerBlock" style="min-width:100%;">
+				<tbody class="mcnDividerBlockOuter">
+				<tr>
+					<td class="mcnDividerBlockInner"
+					    style="min-width: 100%; padding: 9px 18px 0px;">
+						<table class="mcnDividerContent" border="0" cellpadding="0"
+						       cellspacing="0" width="100%" style="min-width:100%;">
+							<tbody>
+							<tr>
+								<td>
+									<span></span>
+								</td>
+							</tr>
+							</tbody>
+						</table>
+					</td>
+				</tr>
+				</tbody>
+			</table>
+			<!--TEXT CENTERED-->
+			<table border="0" cellpadding="0" cellspacing="0" width="100%"
+			       class="mcnTextBlock" style="min-width:100%;">
+				<tbody class="mcnTextBlockOuter">
+				<tr>
+					<td valign="top" class="mcnTextBlockInner" style="padding-top:9px;">
+
+						<table align="left" border="0" cellpadding="0" cellspacing="0"
+						       style="max-width:100%; min-width:100%;" width="100%"
+						       class="mcnTextContentContainer">
+							<tbody>
+							<tr>
+
+								<td valign="top" class="mcnTextContent" style="padding: 0 18px 9px;">
+									<p>Bonjour <?php //TODO: PSEUDO ?>,</p>
+									<p>tu viens d'acheter 1 <?php //TODO: NAME OF GIFT (si plusieurs + ' et 1 '...) ?> grâce à ton solde de kartCoins.</p>
+									<p>Tu as été débité de <?php //TODO:NB KC ?> kartCoins et il te reste donc <?php //TODO:SOLDE KC ?> kartCoins dans ta cagnotte.</p>
+									<p>Tu peux venir dès aujourd'hui retirer ton achat au circuit, en donnant ton pseudo ou ton email à l'accueil.</p>
+									<p>N'oublies pas que tu peux gagner facilement des kartCoins en invitant des amis à
+										télécharger l'application Loc'karting sur leur mobile. Ils gagneront
+										<?php //TODO:NB KC FILLEUL ?> et toi tu gagneras <?php //TODO:NB KC PARRAIN ?> après qu'ils aient effectué leur 1ere course.</p>
+								</td>
+							</tr>
+							</tbody>
+						</table>
+
+					</td>
+				</tr>
+				</tbody>
+			</table>
+		</td>
+		<!--END CONTENT-->
+	</tr>
+</table>
