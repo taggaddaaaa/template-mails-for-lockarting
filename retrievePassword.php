36c9216e1ead1ec1d24a2c3e490d16f8237c460d
taggaddaaaa <sabinecaizergues@hotmail.com>
2019-06-25 17:16:27 +0200

    Add retrievePassword page with its own GIF

---
+++ retrievePassword.php
@@ -0,0 +1,177 @@
+<table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" class="templateContainer">
+    <tr>
+        <!--CONTENT-->
+        <td valign="top" class="bodyContainer">
+            <!--TITLE-->
+            <table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnTextBlock" style="min-width:100%;">
+                <tbody class="mcnTextBlockOuter">
+                <tr>
+                    <td valign="top" class="mcnTextBlockInner" style="padding-top:9px;">
+                        <table align="left" border="0" cellpadding="0" cellspacing="0"
+                               style="max-width:100%; min-width:100%;" width="100%" class="mcnTextContentContainer">
+                            <tbody>
+                            <tr>
+                                <td valign="top" class="mcnTextContent" style="padding: 0 18px 9px;">
+                                    <h4>Qui n'a plus toute sa tête sous son casque?</h4>
+                                    <h1>Bonjour <?php //TODO: PSEUDO ?></h1>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <!--SPACE-->
+            <table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnDividerBlock" style="min-width:100%;">
+                <tbody class="mcnDividerBlockOuter">
+                <tr>
+                    <td class="mcnDividerBlockInner"
+                        style="min-width: 100%; padding: 27px 18px 0px;">
+                        <table class="mcnDividerContent" border="0" cellpadding="0"
+                               cellspacing="0" width="100%" style="min-width:100%;">
+                            <tbody>
+                            <tr>
+                                <td>
+                                    <span></span>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <!--IMAGES-->
+            <table border="0" cellpadding="0" cellspacing="0" width="100%"
+                   class="mcnImageGroupBlock">
+                <tbody class="mcnImageGroupBlockOuter">
+                <!--1 img centered-->
+                <tr>
+                    <td valign="top" style="padding:9px" class="mcnImageGroupBlockInner">
+
+                        <table align="left" width="564" border="0" cellpadding="0"
+                               cellspacing="0" class="mcnImageGroupContentContainer">
+                            <tbody>
+                            <tr>
+                                <td class="mcnImageGroupContent" valign="top"
+                                    style="padding-left: 9px; padding-top: 0; padding-bottom: 0;">
+
+                                    <img alt="mario kart"
+                                         src="img/mdpLost.gif"
+                                         width="564"
+                                         style="max-width:564px; max-height:400px; padding-bottom: 0;"
+                                         class="mcnImage">
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <!--SPACE-->
+            <table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnDividerBlock" style="min-width:100%;">
+                <tbody class="mcnDividerBlockOuter">
+                <tr>
+                    <td class="mcnDividerBlockInner" style="min-width: 100%; padding: 9px 18px 0px;">
+                        <table class="mcnDividerContent" border="0" cellpadding="0"
+                               cellspacing="0" width="100%" style="min-width:100%;">
+                            <tbody>
+                            <tr>
+                                <td>
+                                    <span></span>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <!--TEXT CENTERED-->
+            <table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnTextBlock" style="min-width:100%;">
+                <tbody class="mcnTextBlockOuter">
+                <tr>
+                    <td valign="top" class="mcnTextBlockInner" style="padding-top:9px;">
+
+                        <table align="left" border="0" cellpadding="0" cellspacing="0"
+                               style="max-width:100%; min-width:100%;" width="100%" class="mcnTextContentContainer">
+                            <tbody>
+                            <tr>
+                                <td valign="top" class="mcnTextContent" style="padding: 0 18px 9px;">
+                                    <p>
+                                        Voici ton nouveau mot de passe:
+                                    </p>
+                                    <strong>
+                                        <?php //TODO:NewMDP ?>
+                                    </strong>
+                                    <p>Pour des raisons de sécurité, pense à le changer rapidement.</p>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <!--SPACE-->
+            <table border="0" cellpadding="0" cellspacing="0" width="100%"
+                   class="mcnDividerBlock" style="min-width:100%;">
+                <tbody class="mcnDividerBlockOuter">
+                <tr>
+                    <td class="mcnDividerBlockInner"
+                        style="min-width: 100%; padding: 27px 18px 0px;">
+                        <table class="mcnDividerContent" border="0" cellpadding="0"
+                               cellspacing="0" width="100%" style="min-width:100%;">
+                            <tbody>
+                            <tr>
+                                <td>
+                                    <span></span>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <!--TEXT CENTERED-->
+            <table border="0" cellpadding="0" cellspacing="0" width="100%"
+                   class="mcnTextBlock" style="min-width:100%;">
+                <tbody class="mcnTextBlockOuter">
+                <tr>
+                    <td valign="top" class="mcnTextBlockInner" style="padding-top:9px;">
+                        <table align="left" border="0" cellpadding="0" cellspacing="0"
+                               style="max-width:100%; min-width:100%;" width="100%"
+                               class="mcnTextContentContainer">
+                            <tbody>
+                            <tr>
+                                <td valign="top" class="mcnTextContent"
+                                    style="padding: 0 18px 9px;">
+                                    <p>
+                                        Bonne courses avec nous!
+                                    </p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="top" class="mcnTextContent"
+                                    style="padding: 0 18px 9px;">
+                                    <p>
+                                        L’équipe Loc'karting.
+                                    </p>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </td>
+        <!--END CONTENT-->
+    </tr>
+</table>
